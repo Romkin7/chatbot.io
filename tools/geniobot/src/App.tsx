bbec47c69c4507048ebf7ba187f5bc3f719dfe5d
Roman Tuomisto <romantuomisto@gmail.com>
2021-06-26 15:17:23 +0300

    adds Header in App.tsx

--- tools/geniobot/src/App.tsx
+++ tools/geniobot/src/App.tsx
@@ -1,11 +1,16 @@
 import React, { FC } from 'react';
+import Header from './components/Header/Header';
 import Chat from './components/Chat/Chat';
 import './App.scss';
 
 const App: FC = () => {
 	return (
 		<>
-			<h1>Chatbot.io</h1>
+			<Header
+				headerText="geniobot.io"
+				logoSrc="https://res.cloudinary.com/geniobot-io/image/upload/v1624693972/Logo_plmy4d.svg"
+				logoAlt="geniobot.io logo"
+			/>
 			<Chat />
 		</>
 	);

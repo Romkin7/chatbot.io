86f7445df23933d2a4709c1bee96c343d8573fb0
Yuliia Matvi <jualiasha@gmail.com>
2021-06-28 18:39:34 +0300

    sidebar styles added

--- tools/dashboard/src/Components/Header/Header.tsx
+++ tools/dashboard/src/Components/Header/Header.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { ILogo } from 'app-shared-types';
+const companylogo: ILogo = {
+	src: 'https://res.cloudinary.com/geniobot-io/image/upload/v1624693972/telia-logo_rnc4wl.png',
+	alt: 'Telia logo',
+};
 
-const Header = () => {
-	return <header>This is Header content</header>;
+const Header: FC = () => {
+	return (
+		<header>
+			<section className="companylogo">
+				<img src={companylogo.src} alt={companylogo.alt} />
+			</section>
+			<section className="subscriptionplan">subscriptionplan</section>
+			<section className="specification">specification</section>
+		</header>
+	);
 };
 
 export default Header;
